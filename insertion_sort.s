f9c9489e01f3e8b6ceb592d5ef0c80311728df78
Jack Lambert <91634075+jacklambert1@users.noreply.github.com>
2023-02-21 00:14:29 -0500

    makin' it work you know how it be

--- insertion_sort.s
+++ insertion_sort.s
@@ -24,16 +24,16 @@ main
 			;		0x200 contains prev node addr
 			;		0x204 contains data element
 			;		0x208 contain next node addr
-
+			
 			mov		r6, r5 ;  initialize prev to r5 (head node)
 			mov		r0, #0 ;  intialize r0 as 0 register
-
-			str r0, [r5] 		;head.prev = 0 (NULL)
-			add r7, r5, #32		;allocate nextnode addr in r7
-			str r7, [r5, #8]	;head.next = r7 
-			; need strictly head and pointer nodes => will be empty and exist to avoid any
-			; invalid memory accesses
-
+			
+			str		r0, [r5] 		;head.prev = 0 (NULL)
+			add		r7, r5, #32		;allocate nextnode addr in r7
+			str		r7, [r5, #8]	;head.next = r7
+			;		need strictly head and pointer nodes => will be empty and exist to avoid any
+			;		invalid memory accesses
+			
 			mov		r2, #0 ; use r2 as loop counter;
 			;		r5 will forever hold the head address
 			;		r7 = curr node addr
@@ -56,12 +56,12 @@ end_loop
 			;		need to set tail node and clean up hanging pointers
 			;sub		r7, r7, #32  ; need to deincrement by 32 bc insert_element increments end
 			;		curr = tail
-			str 	r6, [r7]	 ; tail.prev = last inserted node
+			str		r6, [r7]	 ; tail.prev = last inserted node
 			str		r0, [r7, #8] ; tail.next = 0 (NULL)
 			mov		r10, r7      ; r10 will hold the tail address
-			mov		r6, r5		 ; set r6 = head
-			mov		r7, r5		
-			b		done
+			ldr		r7, [r5, #8]
+			mov		r6, r5
+			b		sort
 			
 			;#################################################################################
 			;		Add insert, swap, delete functions
@@ -80,8 +80,10 @@ insert_element
 			
 sort
 			;		IMPLEMENT PROCEDURES TO SORT NEWLY FORMED DOUBLY LINKED LIST HERE
-			cmp		r7, r5      ; check that curr != head
+			cmp		r6, r5      ; check that curr != head
 			beq		skip
+			cmp		r7, r10
+			beq		done
 			ldr		r1, [r6, #4] ; r1 = prev.val
 			ldr		r2, [r7, #4] ; r2 = curr.val
 			cmp		r1, r2       ; if r1 (prior val) > r2 (curr val) swap
@@ -90,27 +92,25 @@ sort
 			b		sort
 			
 			
-swap ;swap curr and prev node.
-
+swap ;swap curr and prev node.	
 			mov		r12, r7 		; r12 = temp_curr = curr
 			mov		r11, r6 		; r11 =temp_prev = prev
 			ldr		r9, [r11]		; r9 = temp_prev.prev
 			ldr		r13, [r12, #8] 	; r13 = temp_curr.next
 			str		r12, [r9, #8]	; temp_prev.prev.next = curr
 			str		r11, [r13]     	; temp_curr.next.prev = temp_prev
-
+			
 			str		r9, [r12]		; temp_curr.prev = temp_prev.prev
 			str		r12, [r11]		; temp_prev.prev = temp_curr
 			str		r13, [r11, #8]  ; temp_prev.next = temp_curr.next
 			str		r11, [r12, #8]	; temp_curr.next = temp_prev
 			
-			mov		r7, r6			; curr = prev
+			;mov		r7, r6			; curr = prev
 			mov		r6, r9			; prev = prev.prev
-			mov		r15, r14		; return			
-
+			;mov		r15, r14		; return
+			b		sort
+			
 skip
-			cmp		r7, r10			; if curr == tail done
-			beq		done
 			mov		r6, r7			; prev = curr
 			ldr		r7, [r7, #8]    ; curr = curr.next
 			b		sort
